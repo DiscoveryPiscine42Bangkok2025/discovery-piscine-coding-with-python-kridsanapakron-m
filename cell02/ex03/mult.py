c7a00811086d70db5526745ed6afa3894c63001c
kridsanapakron <141341438+kridsanapakron@users.noreply.github.com>
2025-09-14 20:21:57 +0700

    F_c2

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -3,9 +3,9 @@ def main(number1, number2):
     total = number1 * number2
     print(str(number1) + " X " + str(number2) + " = " + str(total))
     if total == 0:
-        print("This number is both positive and negative.")
+        print("This result is both positive and negative.")
     elif total < 0:
-        print("This number is negative.")
+        print("This result is negative.")
     else:
-        print("This number is positive.")
+        print("This result is positive.")
 main(int(input("Enter the first number: \n")), int(input("Enter the second number: \n")))
