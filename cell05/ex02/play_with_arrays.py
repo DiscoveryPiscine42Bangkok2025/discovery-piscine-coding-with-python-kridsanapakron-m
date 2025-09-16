6eb3ec9425f524342f5759f9b29db7fb1255ce9a
kridsanapakron <dgsx.64854@gmail.com>
2025-09-16 20:37:00 +0700

    fix some

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -2,6 +2,6 @@
 def main():
     old_list = [2, 8, 9, 48, 8, 22, -12, 2]
     new_list = [x + 2 for x in old_list if x > 5]
-    print("Original array: " + str(old_list))
-    print("New array: " + str(new_list))
+    print(old_list)
+    print(new_list)
 main()
